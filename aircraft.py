7e2465659128543628b00cae73d5634cfca8d333
ericc711 <eric.castillo.batet@estudiantat.upc.edu>
2026-04-14 11:46:13 +0200

    clas

---
+++ aircraft.py
@@ -0,0 +1,7 @@
+class Aircraft:
+    def __init__(self, aircraft_id, airline, origin, time):
+
+        self.id = aircraft_id
+        self.airline = airline
+        self.origin = origin
+        self.time = time
