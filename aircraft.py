40c0fbf951a20a9d992465f6c49a4e5c843ea7ae
ericc711 <eric.castillo.batet@estudiantat.upc.edu>
2026-04-21 11:44:15 +0200

    cosa

--- aircraft.py
+++ aircraft.py
@@ -6,8 +6,6 @@ class Aircraft:
         self.origin = origin
         self.time = time
 
-import os
-
 #Funcio 1
 def LoadArrivals(filename):
     lista_aviones = []
@@ -17,8 +15,9 @@ def LoadArrivals(filename):
         return lista_aviones
 
     # Abrimos y leemos las líneas
-    with open(filename, "r") as f:
-        lineas = f.readlines()
+    f = open(filename, "r")
+    lineas = f.readlines()
+    f.close()
 
     # while
     # Empezamos en z = 1 para saltarnos la cabecera
@@ -69,7 +68,7 @@ def PlotArrivals (aircrafts):
             hora = int(a.time.split(':')[0])
             hora_at.append(hora)
         except (ValueError, AttributeError):
-            print("Linea",i+1," con errores de formato")
+            print("Linea",[i]+1," con errores de formato")
         i +=1
     #creem grafic 24 barres
     plt.hist(hora_at, bins=range(25), edgecolor='black', align='left')
@@ -88,23 +87,23 @@ def SaveFlights(aircrafts, filename):
         print("Error: Lista de vuelos vacia")
         return
     try:
-        with open(filename, "w") as f:
-            f.write("AIRCRAFT ORIGIN ARRIVAL AIRLINE\n")
+        f = open(filename, "w"):
+        f.write("AIRCRAFT ORIGIN ARRIVAL AIRLINE/n")
 
-            i = 0
-            while i < len(aircrafts):
-                a = aircrafts[i]
-                a_id = a.id if a.id else "-"
-                a_origin = a.origin if a.origin else "-"
-                a_time = a.time if a.time else "-"
-                a_airline = a.airline if a.airline else "-"
-
-                f.write(f"{a_id} {a_origin} {a_time} {a_airline}\n")
-                i += 1
+        i=0
+        while i < len(aircrafts):
+            a = aircrafts[i]
+            #fem servir les classes per escriure linies
+            a_id = a.id if a.id else "''"
+            a_origin = a.origin if a.origin else "''"
+            a_time = a.time if a.time else "''"
+            a_airline = a.airline if a.airline else "''"
+            f.write(f"{a_id} {a_origin} {a_time} {a_airline}\n")
+            i +=1
 
         return 0
     #retornem error si n'hi ha
-    except Exception as e:
+    except exception as e:
         print(f"Ha ocurrido un error al guardar el archivo: {e}")
         return -1
 
@@ -143,126 +142,127 @@ def PlotAirlines(aircrafts):
     plt.title('Vuelos por Compañía (Llegadas a LEBL)')
     plt.show()
 
-# Función 5
-from airport import IsSchengenAirport  # Necesitamos importar esta función también
+    # Función 5
+    import matplotlib.pyplot as plt
+    from airport import IsSchengenAirport  # Necesitamos importar esta función también
 
-def PlotFlightsType(aircrafts):
-    # Recibe una lista de vuelos y muestra en una gráfica los schengen y los no schengen
-    # Si la lista está vacia se muestra un error en la pantalla:
+    def PlotFlightsType(aircrafts):
+        # Recibe una lista de vuelos y muestra en una gráfica los schengen y los no schengen
+        # Si la lista está vacia se muestra un error en la pantalla:
 
-    if len(aircrafts) == 0:
-        print("Error: Aircraft list is empty, therefore cannot generate plot")
-        return  # Salimos de la function para que no muestre nada más
+        if len(aircrafts) == 0:
+            print("Error: Aircraft list is empty, therefore cannot generate plot")
+            return  # Salimos de la function para que no muestre nada más
 
-    schengen_count = 0
-    non_schengen_count = 0
+        schengen_count = 0
+        non_schengen_count = 0
 
-    i = 0
-    while i < len(aircrafts):
-        if IsSchengenAirport(aircrafts[i].origin):
-            schengen_count += 1
-        else:
-            non_schengen_count += 1
-        i += 1
+        i = 0
 
-    # Creamos el gráfico con forma "stacked bars"
-    labels = ['Arrivals']  # Solo necesitamos esta columna, ya que las dos salidas van encima.
-
-    fig, ax = plt.subplots()
-
-    ax.bar(labels, [schengen_count], label='Schengen', color='blue')
-
-    # Para que se apilen las gráficas el parámetro 'bottom' empieza donde acaba la otra:
-    ax.bar(labels, [non_schengen_count], bottom=[schengen_count], label='No Schengen', color='lightcoral')
-
-    ax.set_ylabel('Number of flights')
-    ax.set_title('Schengen vs Non-Schengen Arrivals')
-    ax.legend()  # Esto muestra la leyenda de colores
-
-    plt.show()
-
-# Función 6
-from airport import LoadAirports
-
-def MapFlights(aircrafts):
-    # Muestra en Google Earth la trayectoria de todos los vuelos de la lista, desde su origen hasta LEBL.
-    if len(aircrafts) == 0:
-        print("Error: The list of aircrafts is empty. Map cannot be generated.")
-        return
-
-    airports_list = LoadAirports("Airports.txt")
-    if not airports_list:
-        print("Error: No se han podido cargar los aeropuertos")
-        return
-    # Coordenadas de Barcelona LEBL
-    lebl_lat = 41.297445
-    lebl_lon = 2.0832941
-
-    f = open("flights_map.kml", "w")
-
-    f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    f.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
-    f.write('<Document>\n')
-
-    # Estilos KML (Azul para Schengen, Rojo para no Schengen)
-    f.write('  <Style id="schengenStyle">\n')
-    f.write('    <LineStyle>\n')
-    f.write('      <color>ffff0000</color>\n')
-    f.write('      <width>2</width>\n')
-    f.write('    </LineStyle>\n')
-    f.write('  </Style>\n')
-    f.write('  <Style id="nonSchengenStyle">\n')
-    f.write('    <LineStyle>\n')
-    f.write('      <color>ff0000ff</color>\n')
-    f.write('      <width>2</width>\n')
-    f.write('    </LineStyle>\n')
-    f.write('  </Style>\n')
-
-    i = 0
-
-    while i < len(aircrafts):
-        ac = aircrafts[i]
-
-        origin_lat = 0.0
-        origin_lon = 0.0
-        found = False
-
-        # Buscamos las coordenadas del aeropuerto con la lista cargada
-        j = 0
-        while j < len(airports_list) and not found:
-
-            if airports_list[j].code == ac.origin:
-                origin_lat = airports_list[j].coordinates[0]
-                origin_lon = airports_list[j].coordinates[1]
-                found = True
-            j = j+1
-
-        # Dibujamos la ruta si encontramos el aeropuerto de origen
-        if found:
-            if IsSchengenAirport(ac.origin):
-                style = "#schengenStyle"
+        while i < len(aircrafts):
+            if IsSchengenAirport(aircrafts[i].origin):
+                schengen_count = schengen_count + 1
             else:
-                style = "#nonSchengenStyle"
+                non_schengen_count = non_schengen_count + 1
+            i = i + 1
 
-            f.write('  <Placemark>\n')
-            f.write('    <name>Route ' + ac.origin + ' - LEBL (' + ac.airline + ')</name>\n')
-            f.write('    <styleUrl>' + style + '</styleUrl>\n')
-            f.write('    <LineString>\n')
-            f.write('      <altitudeMode>clampToGround</altitudeMode>\n')
-            f.write('      <extrude>1</extrude>\n')
-            f.write('      <tessellate>1</tessellate>\n')
-            f.write('      <coordinates>\n')
+        # Creamos el gráfico con forma "stacked bars"
+        labels = ['Arrivals']  # Solo necesitamos esta columna, ya que las dos salidas van encima.
 
-            f.write('        ' + str(origin_lon) + ',' + str(origin_lat) + '\n')
-            f.write('        ' + str(lebl_lon) + ',' + str(lebl_lat) + '\n')
-            f.write('      </coordinates>\n')
-            f.write('    </LineString>\n')
-            f.write('  </Placemark>\n')
+        fig, ax = plt.subplots()
 
-        i = i+1
+        ax.bar(labels, [schengen_count], label='Schengen', color='blue')
 
-    f.write('</Document>\n')
-    f.write('</kml>\n')
-    f.close()
+        # Para que se apilen las gráficas el parámetro 'bottom' empieza donde acaba la otra:
+        ax.bar(labels, [non_schengen_count], bottom=[schengen_count], label='No Schengen', color='lightcoral')
 
-    print("El mapa se ha guardado correctamente como 'flights_map.kml'")
+        ax.set_ylabel('Number of flights')
+        ax.set_title('Schengen vs Non-Schengen Arrivals')
+        ax.legend()  # Esto muestra la leyenda de colores
+
+        plt.show()
+
+    # Función 6
+    from airport import LoadAirports
+
+    def MapFlights(aircrafts):
+        # Muestra en Google Earth la trayectoria de todos los vuelos de la lista, desde su origen hasta LEBL.
+        if len(aircrafts) == 0:
+            print("Error: The list of aircrafts is empty. Map cannot be generated.")
+            return
+
+        airports_list = LoadAirports("Airports.txt")
+
+        # Coordenadas de Barcelona LEBL
+        lebl_lat = 41.297445
+        lebl_lon = 2.0832941
+
+        f = open("flights_map.kml", "w")
+
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        f.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
+        f.write('<Document>\n')
+
+        # Estilos KML (Azul para Schengen, Rojo para no Schengen)
+        f.write('  <Style id="schengenStyle">\n')
+        f.write('    <LineStyle>\n')
+        f.write('      <color>ffff0000</color>\n')
+        f.write('      <width>2</width>\n')
+        f.write('    </LineStyle>\n')
+        f.write('  </Style>\n')
+
+        f.write('  <Style id="nonSchengenStyle">\n')
+        f.write('    <LineStyle>\n')
+        f.write('      <color>ff0000ff</color>\n')
+        f.write('      <width>2</width>\n')
+        f.write('    </LineStyle>\n')
+        f.write('  </Style>\n')
+
+        i = 0
+
+        while i < len(aircrafts):
+            ac = aircrafts[i]
+
+            origin_lat = 0.0
+            origin_lon = 0.0
+            found = False
+
+            # Buscamos las coordenadas del aeropuerto con la lista cargada
+            j = 0
+            while j < len(airports_list) and not found:
+
+                if airports_list[j].code == ac.origin:
+                    origin_lat = airports_list[j].coordinates[0]
+                    origin_lon = airports_list[j].coordinates[1]
+                    found = True
+                j = j+1
+
+            # Dibujamos la ruta si encontramos el aeropuerto de origen
+            if found:
+                if IsSchengenAirport(ac.origin):
+                    style = "#schengenStyle"
+                else:
+                    style = "#nonSchengenStyle"
+
+                f.write('  <Placemark>\n')
+                f.write('    <name>Route ' + ac.origin + ' - LEBL (' + ac.airline + ')</name>\n')
+                f.write('    <styleUrl>' + style + '</styleUrl>\n')
+                f.write('    <LineString>\n')
+                f.write('      <altitudeMode>clampToGround</altitudeMode>\n')
+                f.write('      <extrude>1</extrude>\n')
+                f.write('      <tessellate>1</tessellate>\n')
+                f.write('      <coordinates>\n')
+
+                f.write('        ' + str(origin_lon) + ',' + str(origin_lat) + '\n')
+                f.write('        ' + str(lebl_lon) + ',' + str(lebl_lat) + '\n')
+                f.write('      </coordinates>\n')
+                f.write('    </LineString>\n')
+                f.write('  </Placemark>\n')
+
+            i = i+1
+
+        f.write('</Document>\n')
+        f.write('</kml>\n')
+        f.close()
+
+        print("El mapa se ha guardado correctamente como 'flights_map.kml'")
